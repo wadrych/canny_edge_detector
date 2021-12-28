0c7a0bf086496ff1d36ca1f39338cf85d9288ea3
wadrych <wojciech.adrych@intel.com>
2021-12-28 17:27:29 +0100

    Added calculation of intensity gradient

--- main.py
+++ main.py
@@ -2,6 +2,12 @@ import numpy as np
 import cv2 as cv
 from matplotlib import pyplot as plt
 from PIL import Image
+from scipy import ndimage
+import skimage
+
+
+def rgb2gray(rgb):
+    return np.dot(rgb[..., :3], [0.2125, 0.7154, 0.0721])
 
 
 def convolution(oldimage, kernel):
@@ -12,12 +18,12 @@ def convolution(oldimage, kernel):
     kernel_h = kernel.shape[0]
     kernel_w = kernel.shape[1]
 
-    if (len(oldimage.shape) == 3):
+    if len(oldimage.shape) == 3:
         image_pad = np.pad(oldimage, pad_width=( \
             (kernel_h // 2, kernel_h // 2), (kernel_w // 2, \
                                              kernel_w // 2), (0, 0)), mode='constant', \
                            constant_values=0).astype(np.float32)
-    elif (len(oldimage.shape) == 2):
+    elif len(oldimage.shape) == 2:
         image_pad = np.pad(oldimage, pad_width=( \
             (kernel_h // 2, kernel_h // 2), (kernel_w // 2, \
                                              kernel_w // 2)), mode='constant', constant_values=0) \
@@ -65,13 +71,36 @@ def GaussianFilter(image, sigma):
     return im_filtered.astype(np.uint8)
 
 
+def SobelFilter(img, direction):
+    if direction == 'x':
+        Gx = np.array([[-1, 0, +1], [-2, 0, +2], [-1, 0, +1]])
+        Res = ndimage.convolve(img, Gx)
+    if direction == 'y':
+        Gy = np.array([[-1, -2, -1], [0, 0, 0], [+1, +2, +1]])
+        Res = ndimage.convolve(img, Gy)
+
+    return Res
+
+
+def Normalize(img):
+    # img = np.multiply(img, 255 / np.max(img))
+    img = img / np.max(img)
+    return img
+
+
 def canny(img, threshold1, threshold2):
     # Apply Gaussian filter to smooth the image in order to remove the noise
     sigma = 2.5
     img = GaussianFilter(img, sigma)
 
-    # Find the intensity gradients of the image
+    img = skimage.color.rgb2gray(img)
 
+    gx = SobelFilter(img, 'x')
+    gx = Normalize(gx)
+    gy = SobelFilter(img, 'y')
+    gy = Normalize(gy)
+
+    # Apply gradient magnitude thresholding or lower bound cut-off suppression to get rid of spurious response to edge detection
 
 
     return img
@@ -79,11 +108,12 @@ def canny(img, threshold1, threshold2):
 
 def print_hi(name):
     img = Image.open('images/messi5.jpg')
+
     edges = canny(img, 300, 550)
     # edges = GaussianFilter(img, 2.5)
-    plt.subplot(121), plt.imshow(img, cmap='gray')
+    plt.subplot(121), plt.imshow(img)
     plt.title('Original Image'), plt.xticks([]), plt.yticks([])
-    plt.subplot(122), plt.imshow(edges, cmap='gray')
+    plt.subplot(122), plt.imshow(edges)
     plt.title('Edge Image'), plt.xticks([]), plt.yticks([])
     plt.show()
 
