55225ab846731f908bb9ed29d5329e1f162ca151
wadrych <wojciech.adrych@intel.com>
2021-12-28 16:38:47 +0100

    Added gausian filter

--- main.py
+++ main.py
@@ -1,16 +1,92 @@
-# This is a sample Python script.
+import numpy as np
+import cv2 as cv
+from matplotlib import pyplot as plt
+from PIL import Image
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+def convolution(oldimage, kernel):
+    # image = Image.fromarray(image, 'RGB')
+    image_h = oldimage.shape[0]
+    image_w = oldimage.shape[1]
+
+    kernel_h = kernel.shape[0]
+    kernel_w = kernel.shape[1]
+
+    if (len(oldimage.shape) == 3):
+        image_pad = np.pad(oldimage, pad_width=( \
+            (kernel_h // 2, kernel_h // 2), (kernel_w // 2, \
+                                             kernel_w // 2), (0, 0)), mode='constant', \
+                           constant_values=0).astype(np.float32)
+    elif (len(oldimage.shape) == 2):
+        image_pad = np.pad(oldimage, pad_width=( \
+            (kernel_h // 2, kernel_h // 2), (kernel_w // 2, \
+                                             kernel_w // 2)), mode='constant', constant_values=0) \
+            .astype(np.float32)
+
+    h = kernel_h // 2
+    w = kernel_w // 2
+
+    image_conv = np.zeros(image_pad.shape)
+
+    for i in range(h, image_pad.shape[0] - h):
+        for j in range(w, image_pad.shape[1] - w):
+            # sum = 0
+            x = image_pad[i - h:i - h + kernel_h, j - w:j - w + kernel_w]
+            x = x.flatten() * kernel.flatten()
+            image_conv[i][j] = x.sum()
+    h_end = -h
+    w_end = -w
+
+    if (h == 0):
+        return image_conv[h:, w:w_end]
+    if (w == 0):
+        return image_conv[h:h_end, w:]
+
+    return image_conv[h:h_end, w:w_end]
+
+
+def GaussianFilter(image, sigma):
+    image = np.asarray(image)
+    # print(image)
+    filter_size = 2 * int(4 * sigma + 0.5) + 1
+    gaussian_filter = np.zeros((filter_size, filter_size), np.float32)
+    m = filter_size // 2
+    n = filter_size // 2
+
+    for x in range(-m, m + 1):
+        for y in range(-n, n + 1):
+            x1 = 2 * np.pi * (sigma ** 2)
+            x2 = np.exp(-(x ** 2 + y ** 2) / (2 * sigma ** 2))
+            gaussian_filter[x + m, y + n] = (1 / x1) * x2
+
+    im_filtered = np.zeros_like(image, dtype=np.float32)
+    for c in range(3):
+        im_filtered[:, :, c] = convolution(image[:, :, c], gaussian_filter)
+    return im_filtered.astype(np.uint8)
+
+
+def canny(img, threshold1, threshold2):
+    # Apply Gaussian filter to smooth the image in order to remove the noise
+    sigma = 2.5
+    img = GaussianFilter(img, sigma)
+
+    # Find the intensity gradients of the image
+
+
+
+    return img
 
 
 def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    img = Image.open('images/messi5.jpg')
+    edges = canny(img, 300, 550)
+    # edges = GaussianFilter(img, 2.5)
+    plt.subplot(121), plt.imshow(img, cmap='gray')
+    plt.title('Original Image'), plt.xticks([]), plt.yticks([])
+    plt.subplot(122), plt.imshow(edges, cmap='gray')
+    plt.title('Edge Image'), plt.xticks([]), plt.yticks([])
+    plt.show()
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
